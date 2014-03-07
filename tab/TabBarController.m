9c47b9e31b6adb376639e2a1dbbf76930f451b41
MIYATA Wataru <myt0930@gmail.com>
2014-03-07 19:03:04 +0900

    広告とタブの隙間に1px空きがあったので詰めた

--- tab/TabBarController.m
+++ tab/TabBarController.m
@@ -31,7 +31,7 @@
 	
 	int advertisementPosY = self.tabBar.frame.origin.y;
 
-	self.tabBar.frame = CGRectMake(0, advertisementPosY - 50, self.tabBar.frame.size.width, self.tabBar.frame.size.height);
+	self.tabBar.frame = CGRectMake(0, advertisementPosY - 49, self.tabBar.frame.size.width, self.tabBar.frame.size.height);
 	
 	UIView *adView = [[UIView alloc] initWithFrame:CGRectMake(0, advertisementPosY, 320, 50)];
 	adView.backgroundColor = [UIColor blackColor];
